d22a3291df14f60a56424e0279f342bde9781ede
JasperSchaffer <44Jasper44@gmail.com>
2020-11-07 18:11:48 -0500

    implemented posenet into frontend

--- backend/model/tensorflow.js
+++ backend/model/tensorflow.js
@@ -1,5 +1,11 @@
-import * as tf from '@tensorflow/tfjs';
-import * as tmPose from '@teachablemachine/pose';
+
+// import * as tf from '@tensorflow/tfjs';
+// import * as tmPose from '@teachablemachine/pose';
+
+const tf = require('@tensorflow/tfjs');
+const tmPose = require('@teachablemachine/pose');
+global.fetch = require("node-fetch");
+const { Image, createCanvas } = require('canvas');
     // More API functions here:
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
@@ -7,7 +13,7 @@ import * as tmPose from '@teachablemachine/pose';
     const URL = "https://teachablemachine.withgoogle.com/models/o-TZh3sFa/";
     let model, webcam, ctx, labelContainer, maxPredictions;
 
-    module.exports = async function init() {
+    async function init() {
         const modelURL = URL + "model.json";
         const metadataURL = URL + "metadata.json";
 
@@ -15,57 +21,97 @@ import * as tmPose from '@teachablemachine/pose';
         // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
         // Note: the pose library adds a tmPose object to your window (window.tmPose)
         model = await tmPose.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
 
+        tmPose.load(modelURL, metadataURL).then(mod => {
+            model = mod;
+            maxPredictions = model.getTotalClasses();
+
+        }).catch(err => console.log(err));
+       
         // Convenience function to setup a webcam
-        const size = 200;
-        const flip = true; // whether to flip the webcam
-        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
+        // const size = 200;
+        // const flip = true; // whether to flip the webcam
+        // webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        // await webcam.setup(); // request access to the webcam
+        // await webcam.play();
+       // window.requestAnimationFrame(loop);
 
         // append/get elements to the DOM
-        const canvas = document.getElementById("canvas");
-        canvas.width = size; canvas.height = size;
-        ctx = canvas.getContext("2d");
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
+        // const canvas = document.getElementById("canvas");
+        // canvas.width = size; canvas.height = size;
+        // ctx = canvas.getContext("2d");
+        // labelContainer = document.getElementById("label-container");
+        // for (let i = 0; i < maxPredictions; i++) { // and class labels
+        //     labelContainer.appendChild(document.createElement("div"));
+        // }
     }
 
-    module.exports = async function loop(timestamp) {
+    async function loop(timestamp) {
         webcam.update(); // update the webcam frame
         await predict();
-        window.requestAnimationFrame(loop);
+        //window.requestAnimationFrame(loop);
     }
 
-    module.exports = async function predict() {
+    async function predict() {
         // Prediction #1: run input through posenet
         // estimatePose can take in an image, video or canvas html element
-        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
-        // Prediction 2: run input through teachable machine classification model
+        // const nsfwjs = require('../lib/index');
+    // const { Image, createCanvas } = require('canvas');
+    //     tf.load().then(function (model) {
+    //         console.log("model loaded.");
+    //     const width = 300;
+    //     const height = 300;
+    //     const canvas = createCanvas(width, height);
+    //     const ctx = canvas.getContext('2d');
+    //     const img = new Image()
+    //     img.onload = () => {
+    //       console.log("image loaded.");
+      
+    //       ctx.drawImage(img, 0, 0, width, height);
+      
+    //       // classify
+    //       model.classify(canvas).then(function (predictions) {
+    //         // Classify the image
+    //         console.log('Predictions: ', predictions)
+    //       })
+    //     }
+    //     img.onerror = err => { throw err }
+    //     img.src = '60.jpg';
+      
+      
+    //   });
+        const { pose, posenetOutput } = await model.estimatePose("60.jpg");
+        // // Prediction 2: run input through teachable machine classification model
         const prediction = await model.predict(posenetOutput);
 
         for (let i = 0; i < maxPredictions; i++) {
             const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+                prediction[i].className;
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
-
+        console.log(classPrediction);
+        return classPrediction;
         // finally draw the poses
-        drawPose(pose);
+        //drawPose(pose);
     }
 
-   module.exports = function drawPose(pose) {
-        if (webcam.canvas) {
-            ctx.drawImage(webcam.canvas, 0, 0);
-            // draw the keypoints and skeleton
-            if (pose) {
-                const minPartConfidence = 0.5;
-                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-            }
-        }
-    }
+//    module.exports = function drawPose(pose) {
+//         if (webcam.canvas) {
+//             ctx.drawImage(webcam.canvas, 0, 0);
+//             // draw the keypoints and skeleton
+//             if (pose) {
+//                 const minPartConfidence = 0.5;
+//                 tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+//                 tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+//             }
+//         }
+//     }
+
+
+
+async function main() {
+    await init();
+    predict();
+}
+
+main();
