922520518a240b63ecf30f68d54f41a4d04ac1f0
ologush <17ojl@queensu.ca>
2020-11-07 15:56:02 -0500

    Video is displayed

--- frontend/src/Components/RecordComponent.jsx
+++ frontend/src/Components/RecordComponent.jsx
@@ -1,28 +1,100 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-import { Button } from "@blueprintjs/core";
+import ReactDOM from 'react-dom';
 
-import VideoRecorder from 'react-video-recorder'
+import Button from "@material-ui/core/Button"
+
+const constraints = {
+    audio: false,
+    video: {
+        facingMode: "user"
+    }
+};
+
+//import VideoRecorder from 'react-video-recorder'
 
 function RecordComponent(props) {
+
+    let mediaRecorder;
+
+    const videoRef = useRef();
+
+    const [isRecording, setIsRecording] = useState(true);
+
+    const [videoStream, setVideoStream] = useState();
 
 
     const handleClick = () => {
 
+        
+
+        console.log('a')
+
+        
+
+        let facingMode = "user";
+        let constraints = {
+            audio: false,
+            video: {
+                facingMode: facingMode
+            }
+        };
+
+        
+
+        navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+            setIsRecording(true);
+            setVideoStream(stream);
+
+            let video = document.getElementById('vid');
+            video.srcObject = stream;
+
+        })
     }
+
+    useEffect(() => {
+
+        mediaRecorder = new mediaRecorder(videoStream, { mimeType: "video/mp4" });
+        mediaRecorder.ondataavailable = handleDataAvailable;
+        mediaRecorder.start();
+
+    }, [videoStream]);
 
     return (
         <div>
-            <Button className={"docs-wiggle"} icon="video">
+            <Button onClick={handleClick}>
                 Click to Record!
             </Button>
-            <VideoRecorder onRecordingComplete={(videoBlob) => {
-                console.log(videoBlob);
-            }} />
+
+            {
+                isRecording ? (
+                    <video id="vid" autoPlay>
+                        
+                    </video>
+                ) : ("")
+            }
+            
         </div>
        
     )
 
 }
+
+// function RecordComponent(props) {
+//     return (
+//         <div>
+//         {
+//             navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+                
+//                 (
+//                     <video autoplay >
+//                         <source src={stream} type="video/mp4" />
+//                     </video>
+//                 )
+//             })
+//         }
+//         </div>
+//     )
+// }
 
 export default RecordComponent;
